849df002d929980b7a804250346f8f1981f4fb70
Aryan Silawal <silawalaryan16@gmail.com>
2025-08-04 22:04:58 +0545

    feature/rotation Updated the rotation using rk4

--- src/components/ObjectCluster.jsx
+++ src/components/ObjectCluster.jsx
@@ -1,11 +1,108 @@
-import { useState } from "react";
+// import { useState } from "react";
 
+// import * as THREE from "three";
+// import { useFrame, useLoader } from "@react-three/fiber";
+
+// import TestObject from "./TestObject.jsx";
+
+// import { GRAVITATIONAL_CONSTANT } from "../utils/constants.js";
+
+// export default function ObjectCluster() {
+//   const sunTexture = useLoader(THREE.TextureLoader, "/assets/sun_surface.jpg");
+//   const planetTexture = useLoader(
+//     THREE.TextureLoader,
+//     "/assets/earth_surface_day.jpg"
+//   );
+
+//   const [objects, setObjects] = useState([
+//     {
+//       id: 1,
+//       mass: 1000000,
+//       radius: 50,
+//       position: new THREE.Vector3(0, 0, 0),
+//       velocity: new THREE.Vector3(0, 0, 0),
+//       acceleration: new THREE.Vector3(0, 0, 0),
+//       texture: sunTexture,
+//     },
+//     {
+//       id: 2,
+//       mass: 1,
+//       radius: 10,
+//       position: new THREE.Vector3(250, 0, 0),
+//       velocity: new THREE.Vector3(0, 0, 63.2),
+//       acceleration: new THREE.Vector3(0, 0, 0),
+//       //   color: "#ecb212",
+//       texture: planetTexture,
+//     },
+//   ]);
+
+//   useFrame((state, delta) => {
+//     setObjects((prevObjects) => {
+//       const updatedObjects = prevObjects.map((eachObject, identifierIndex) => {
+//         let resultantAcceleration = new THREE.Vector3(0, 0, 0);
+
+//         prevObjects.forEach((otherObject, index) => {
+//           if (identifierIndex !== index) {
+//             const resultantVector = otherObject.position
+//               .clone()
+//               .sub(eachObject.position);
+//             const distSq = resultantVector.lengthSq();
+
+//             if (distSq > 0.0001) {
+//               const forceDir = resultantVector.normalize();
+//               const accelMag =
+//                 (GRAVITATIONAL_CONSTANT * otherObject.mass) / distSq;
+//               resultantAcceleration.add(forceDir.multiplyScalar(accelMag));
+//             }
+//           }
+//         });
+
+//         const newPosition = eachObject.position
+//           .clone()
+//           .add(eachObject.velocity.clone().multiplyScalar(delta))
+//           .add(
+//             resultantAcceleration.clone().multiplyScalar(0.5 * delta * delta)
+//           );
+
+//         let newVelocity = eachObject.velocity
+//           .clone()
+//           .add(resultantAcceleration.clone().multiplyScalar(delta));
+
+//         return {
+//           ...eachObject,
+//           position: newPosition,
+//           velocity: newVelocity,
+//           acceleration: resultantAcceleration,
+//         };
+//       });
+
+//       return updatedObjects;
+//     });
+//   });
+
+//   return (
+//     <>
+//       {objects.map((eachObject) => (
+//         <TestObject
+//           key={eachObject.id}
+//           texture={eachObject.texture}
+//           radius={eachObject.radius}
+//           position={eachObject.position}
+//         />
+//       ))}
+//     </>
+//   );
+// }
+
+
+//use of rk4 method
+import { useState } from "react";
 import * as THREE from "three";
-import { useFrame, useLoader } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
 
 import TestObject from "./TestObject.jsx";
-
 import { GRAVITATIONAL_CONSTANT } from "../utils/constants.js";
+import { useFixedstep } from "../hooks/useFixedstep";
 
 export default function ObjectCluster() {
   const sunTexture = useLoader(THREE.TextureLoader, "/assets/sun_surface.jpg");
@@ -31,54 +128,76 @@ export default function ObjectCluster() {
       position: new THREE.Vector3(250, 0, 0),
       velocity: new THREE.Vector3(0, 0, 63.2),
       acceleration: new THREE.Vector3(0, 0, 0),
-      //   color: "#ecb212",
       texture: planetTexture,
     },
   ]);
 
-  useFrame((state, delta) => {
-    setObjects((prevObjects) => {
-      const updatedObjects = prevObjects.map((eachObject, identifierIndex) => {
-        let resultantAcceleration = new THREE.Vector3(0, 0, 0);
-
-        prevObjects.forEach((otherObject, index) => {
-          if (identifierIndex !== index) {
-            const resultantVector = otherObject.position
-              .clone()
-              .sub(eachObject.position);
-            const distSq = resultantVector.lengthSq();
-
-            if (distSq > 0.0001) {
-              const forceDir = resultantVector.normalize();
-              const accelMag =
-                (GRAVITATIONAL_CONSTANT * otherObject.mass) / distSq;
-              resultantAcceleration.add(forceDir.multiplyScalar(accelMag));
-            }
-          }
-        });
-
-        const newPosition = eachObject.position
-          .clone()
-          .add(eachObject.velocity.clone().multiplyScalar(delta))
-          .add(
-            resultantAcceleration.clone().multiplyScalar(0.5 * delta * delta)
-          );
-
-        let newVelocity = eachObject.velocity
-          .clone()
-          .add(resultantAcceleration.clone().multiplyScalar(delta));
-
-        return {
-          ...eachObject,
-          position: newPosition,
-          velocity: newVelocity,
-          acceleration: resultantAcceleration,
-        };
-      });
-
-      return updatedObjects;
+  const computeAcceleration = (position, others) => {
+    let acc = new THREE.Vector3();
+    others.forEach((other) => {
+      const direction = other.position.clone().sub(position);
+      const distSq = direction.lengthSq();
+      if (distSq > 0.0001) {
+        const forceDir = direction.normalize();
+        const accelMag = (GRAVITATIONAL_CONSTANT * other.mass) / distSq;
+        acc.add(forceDir.multiplyScalar(accelMag));
+      }
     });
-  });
+    return acc;
+  };
+
+  const rk4Integrate = (obj, others, dt) => {
+    const pos0 = obj.position.clone();
+    const vel0 = obj.velocity.clone();
+    const acc0 = computeAcceleration(pos0, others);
+
+    const k1v = acc0.clone().multiplyScalar(dt);
+    const k1p = vel0.clone().multiplyScalar(dt);
+
+    const k2pPos = pos0.clone().add(k1p.clone().multiplyScalar(0.5));
+    const k2vVel = vel0.clone().add(k1v.clone().multiplyScalar(0.5));
+    const k2v = computeAcceleration(k2pPos, others).multiplyScalar(dt);
+    const k2p = k2vVel.clone().multiplyScalar(dt);
+
+    const k3pPos = pos0.clone().add(k2p.clone().multiplyScalar(0.5));
+    const k3vVel = vel0.clone().add(k2v.clone().multiplyScalar(0.5));
+    const k3v = computeAcceleration(k3pPos, others).multiplyScalar(dt);
+    const k3p = k3vVel.clone().multiplyScalar(dt);
+
+    const k4pPos = pos0.clone().add(k3p);
+    const k4vVel = vel0.clone().add(k3v);
+    const k4v = computeAcceleration(k4pPos, others).multiplyScalar(dt);
+    const k4p = k4vVel.clone().multiplyScalar(dt);
+
+    const newPosition = pos0
+      .clone()
+      .add(k1p.clone().multiplyScalar(1 / 6))
+      .add(k2p.clone().multiplyScalar(1 / 3))
+      .add(k3p.clone().multiplyScalar(1 / 3))
+      .add(k4p.clone().multiplyScalar(1 / 6));
+
+    const newVelocity = vel0
+      .clone()
+      .add(k1v.clone().multiplyScalar(1 / 6))
+      .add(k2v.clone().multiplyScalar(1 / 3))
+      .add(k3v.clone().multiplyScalar(1 / 3))
+      .add(k4v.clone().multiplyScalar(1 / 6));
+
+    return {
+      ...obj,
+      position: newPosition,
+      velocity: newVelocity,
+      acceleration: computeAcceleration(newPosition, others),
+    };
+  };
+
+  useFixedstep((fixedDelta) => {
+    setObjects((prevObjects) => {
+      return prevObjects.map((obj, i) =>
+        rk4Integrate(obj, prevObjects.filter((_, j) => j !== i), fixedDelta)
+      );
+    });
+  }, 1 / 60);
 
   return (
     <>
@@ -93,3 +212,4 @@ export default function ObjectCluster() {
     </>
   );
 }
+
